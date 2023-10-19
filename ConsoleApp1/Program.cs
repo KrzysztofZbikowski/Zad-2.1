a693addfcab3f340e75d60fccdfac43a8cbeb332
Zbiku <Zbiku@MSI>
2023-10-19 22:42:57 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿class Zad
+{   
+    static void Main(string[] args)
+    {
+        double F, C;
+        Console.WriteLine("Podaj temp. w stopniach Celcjusza");
+        C = double.Parse(Console.ReadLine());
+        F = 32 + (9.0 / 5) * C;
+        Console.WriteLine(F);
+        Console.ReadKey();
+    }
+}
